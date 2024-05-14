c01ab9899bb1e3257ce9d89a4bc5dd2a1c965217
KeipS <kj.pj.sj@gmail.com>
2024-05-15 00:43:13 +0330

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Simplextep',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     description='Simplex implementation with steps.',
     author='Keivan Jamali',
